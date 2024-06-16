92b2741696787b619384d723bb4953d26342b745
Shivam kesarwani <86849374+peaceinshiva@users.noreply.github.com>
2024-06-16 19:48:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+function sendwhatsapt(){
+
+    // var fname,lname,country,subject;
+
+
+    var phonenumber="+918303323232";
+
+   var name= document.getElementById("name").value;
+    var destinat=document.getElementById("destinat").value;
+   var subject =document.getElementById("subject").value;
+    var phone=document.getElementById("Phone").value;
+
+
+if(name==""){
+    alert("Enter your name first")
+}
+else if(destinat==""){
+    alert("Enter your  destination first")
+}
+
+else if(subject==""){
+    alert("Enter Details first")
+}
+
+else if(phone==""){
+    alert("Enter your phone number first")
+}
+else{
+    // alert(phone);
+   
+
+    var url="https://wa.me/" + phonenumber + "?text="+
+    
+    "Book my cab %0a%0aName: "+ name+
+    "%0aDestination: "+ destinat+
+    "%0aDetails: "+subject+
+    "%0aPhone:"+phone
+
+  
+    window.open(url,'_blank').focus();
+}
+
+
+
+
+
+
+
+
+
+
+
+   
+  
+}
+
